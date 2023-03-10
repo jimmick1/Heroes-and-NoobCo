1d2c1198af87b7b6c982193f18c979389b8be335
Игорь Плаксин <jimmick@live.ru>
2023-03-10 16:33:44 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes_and_NoobCo
+{
+    abstract class Weapon
+    {
+        public string damagetype = "";
+        public string name = "";
+        public int basedamage;
+        public int bonus;
+        public int FullDamage {get => (int)basedamage + (int)bonus; set => bonus = value;}
+    }
+    internal class Sword : Weapon
+    {
+        public Sword () : base ()
+        {
+            name = "Sword";
+            damagetype = "physical";
+            basedamage = 5;
+        }
+    }
+    internal class Dagger : Weapon
+    {
+        public Dagger () : base()
+        {
+            name = "Dagger";
+            damagetype = "physical";
+            basedamage = 4;
+        }
+    }
+    internal class Staff : Weapon
+    {
+        public Staff () : base()
+        {
+            name = "Staff";
+            damagetype = "physical";
+            basedamage = 15;
+        }
+    }
+    internal class Cast : Weapon
+    {
+        public Cast () : base()
+        {
+            name = "Cast";
+            damagetype = "magic";
+            basedamage = 0;
+        }
+    }
+    abstract class Hero
+    {
+        internal string name = "";
+        internal int power;
+        internal int agility;
+        internal int health;
+        internal int intellect;
+        
+        internal virtual int Power { get => power; set => power = value; }
+        internal virtual int Health { get => health; set => health = value; }
+        internal virtual string Name { get => name; set => name = value; }
+        internal virtual int Agility { get => agility; set => agility = value; }
+        internal virtual int Intellect { get => intellect; set => intellect = value; }
+
+        internal virtual int HealthRate { get => (int)Health * 4; set => Health = value; }
+        internal virtual int Mana { get => (int)Intellect * 4;  set => Intellect = value; }
+
+        internal virtual int Armor { get => (int)Agility / 2; set => Agility = value; }
+        internal virtual int MageArmor { get => (int)Intellect / 2; set => Intellect = value; }
+        
+        internal Weapon? weapon;
+        public Hero (int power, int agility, int health, int intellect, string name)
+        {
+            this.Power = power;
+            this.Health = health;
+            this.Name = name;
+            this.Agility = agility;
+            this.Intellect = intellect;
+            this.HealthRate = this.Health;
+            this.Mana = this.Intellect;
+            this.Armor = this.Agility;
+            this.MageArmor = this.Intellect;
+        }
+    }
+    class Knight : Hero
+    {
+        internal override int HealthRate { get => base.HealthRate + 15;}
+        internal override int Power  { get => base.Power + 2;}
+        internal override int Armor { get => base.Armor + 2;}
+        Weapon KnightWeapon = new Sword();
+
+        public Knight(int power, int agility, int health, int intellect, string name) :
+            base(power, agility, health, intellect, name)
+        {
+            KnightWeapon.FullDamage = power;
+        }
+    }
+    class Thief : Hero
+    {
+        internal override int Agility { get => base.Agility + 2;}
+        Weapon ThiefWeapon = new Dagger();
+        public Thief(int power, int agility, int health, int intellect, string name) :
+            base(power, agility, health, intellect, name)
+        {           
+            ThiefWeapon.FullDamage = agility;
+        }
+    }
+    class Mage : Hero
+    {
+        internal override int Intellect { get => base.Intellect + 5;}
+        internal override int Mana { get => base.Mana + 25;}
+        internal override int MageArmor { get => base.MageArmor + 2;}
+        Weapon MageWeapon = new Staff();
+        public Mage(int power, int agility, int health, int intellect, string name) :
+            base(power, agility, health, intellect, name)
+        {
+            MageWeapon.FullDamage = intellect;
+            Weapon Cast = new Cast();
+            if (MageWeapon != null)
+            {
+                Cast.basedamage += 10;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Heroes_and_NoobCo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string? hero = Console.ReadLine();
+                if (hero != "hero")
+                    throw new Exception($"You must enter \"hero\" instead of {hero}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return;
+            }
+            List<Hero> heroes = new List<Hero>();
+            while (true)
+            {
+                string? hero = Console.ReadLine();
+                if (hero == "enemy")
+                {
+                    break;
+                }
+                else
+                {
+                    string [] heroesenter = hero.Split(' ', 6).ToArray();
+                    try
+                    {
+                        switch (heroesenter[0])
+                        {
+                            case "knight":
+                                heroes.Add(new Knight(Convert.ToInt32(heroesenter[1]), Convert.ToInt32(heroesenter[2]),
+                                    Convert.ToInt32(heroesenter[3]), Convert.ToInt32(heroesenter[4]), heroesenter[5]));
+                                break;
+                            case "thief":
+                                heroes.Add(new Thief(Convert.ToInt32(heroesenter[1]), Convert.ToInt32(heroesenter[2]),
+                                    Convert.ToInt32(heroesenter[3]), Convert.ToInt32(heroesenter[4]), heroesenter[5]));
+                                break;
+                            case "mage":
+                                heroes.Add(new Mage(Convert.ToInt32(heroesenter[1]), Convert.ToInt32(heroesenter[2]),
+                                    Convert.ToInt32(heroesenter[3]), Convert.ToInt32(heroesenter[4]), heroesenter[5]));
+                                break;
+                            default: throw new Exception("Please check your enter");
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return;
+                    }
+                }
+                
+            }
+            List<Hero> enemies = new List<Hero>();
+            while (true)
+            {
+                string? hero = Console.ReadLine();
+                if (hero == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    string[] heroesenter = hero.Split(' ', 6).ToArray();
+                    try
+                    {
+                        switch (heroesenter[0])
+                        {
+                            case "knight":
+                                enemies.Add(new Knight(Convert.ToInt32(heroesenter[1]), Convert.ToInt32(heroesenter[2]),
+                                    Convert.ToInt32(heroesenter[3]), Convert.ToInt32(heroesenter[4]), heroesenter[5]));
+                                break;
+                            case "thief":
+                                enemies.Add(new Thief(Convert.ToInt32(heroesenter[1]), Convert.ToInt32(heroesenter[2]),
+                                    Convert.ToInt32(heroesenter[3]), Convert.ToInt32(heroesenter[4]), heroesenter[5]));
+                                break;
+                            case "mage":
+                                enemies.Add(new Mage(Convert.ToInt32(heroesenter[1]), Convert.ToInt32(heroesenter[2]),
+                                    Convert.ToInt32(heroesenter[3]), Convert.ToInt32(heroesenter[4]), heroesenter[5]));
+                                break;
+                            default: throw new Exception("Please check your enter");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return;
+                    }
+                }
+
+            }
+        }
+    }
+}
+
